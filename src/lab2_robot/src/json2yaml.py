da8ad602ffff8dec3180cc1a4d8d0daa3a9ce823
Damian Wysokiński <d.wysokinski@stud.elka.pw.edu.pl>
2019-05-04 17:54:33 +0200

    wstepny kod

--- src/lab2_robot/src/json2yaml.py
+++ src/lab2_robot/src/json2yaml.py
@@ -2,3 +2,18 @@ import math
 import yaml
 import json
 
+print 'dziala'
+
+with open('dh_data.json') as jsonFile:
+	dh = json.load(jsonFile)
+
+print 'dziala2'
+
+with open('yaml_data.yaml', 'w') as yamlFile:
+    print 'stworzono plik'
+    for i in dh.keys():
+        row = dh[i]
+        
+        yamlFile.write(i + ':\n')
+        yamlFile.write('   '+ 'length:'+ str(row[1])+'\n')
+        yamlFile.write('   '+ 'xyz:'+ '0.0 '+ str(row[i])+str(row[+'\n')
